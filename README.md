a45c1e626b2c575e999c3d257ca95f889c9308ad
Yamrc <yamrci@outlook.com>
2023-08-25 07:51:24 +0000

    feat: 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # uptime-page
-站点信息监测页，fork自imsyy/site-status
+
+一个基于 UptimeRobot API 的在线状态面板
+
+## 部署
+
+### 安装依赖
+
+```bash
+# 若没有 yarn
+npm install yarn -g
+
+# 安装依赖
+yarn install
+```
+
+### 开发
+
+```bash
+yarn dev
+```
+
+### 构建
+
+```bash
+yarn build
+```
+
+构建后的文件会存储于 dist 目录中。
+
+## 事先准备
+
+- 您需要先到 [UptimeRobot](https://uptimerobot.com/ "UptimeRobot") 添加站点监控，并在 My Settings 页面获取 API Key
+- 您需要拥有一个网站空间，常见的 Nginx / PHP 等空间即可，甚至是阿里云的 OSS 等纯静态空间也行，也可以使用 `Vercel` 或者 `Cloudflare` 直接部署该项目。
+
+## 如何部署：
+- 所有配置均位于.env文件内
+- 将所有文件上传到网站空间
+
+## 鸣谢
+
+ - [site-status](https://github.com/imsyy/site-status) 基于此项目进行修改
---
+++ src/components/footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CustomLink from "@/components/customLink";
+import Package from "../../package.json";
+
+const Footer = () => {
+  // 加载配置
+  const siteIcp = import.meta.env.VITE_SITE_ICP;
+  const copyry = import.meta.env.VITE_COPYR_STARTYEAR;
+  const copyrn = import.meta.env.VITE_COPYR_NAME;
+  const copyrnu = import.meta.env.VITE_COPYR_NAME_URL;
+
+  return (
+    <footer id="footer">
+      <div className="text">
+        <p>
+          Version&nbsp;{Package.version}
+        </p>
+        <p>
+          基于&nbsp;
+          <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
+          &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
+        </p>
+        <p>
+          Copyright&nbsp;&copy;&nbsp;{copyry}&nbsp;-&nbsp;{new Date().getFullYear()}
+          &nbsp;
+          <CustomLink to={copyrnu} text={copyrn} />
+          {siteIcp ? (
+            <React.Fragment>
+              &nbsp;|&nbsp;
+              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
+            </React.Fragment>
+          ) : null}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
